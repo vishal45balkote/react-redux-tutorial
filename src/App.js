66cc851bf4981fb04e666d23847bb304e566cad3
vishal balkote <vishal45balkote@gmail.com>
2021-04-25 17:40:00 +0530

    Added files
    description

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from "react-redux";
+// import { startAction } from './actions/startAction';
+// import { stopAction } from './actions/stopAction';
+import rotateAction from './actions/rotateAction';
+class App extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            className={
+              "App-logo" +
+              (this.props.rotating ? "":" App-logo-paused")
+            }
+            alt="logo"
+            onClick={
+              () => this.props.rotateAction(!this.props.rotating)
+            }
+          />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  rotateAction: (payload) => dispatch(rotateAction(payload))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
